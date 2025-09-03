258e24cebe6fbed36b8d0226e837cfe4f90fd562
jefersoncarr <jefersoncarranzaruiz@gmail.com>
2025-09-02 20:04:21 -0500

    hola

---
+++ 01-HolaPeople/HolaPeople.js
@@ -0,0 +1,40 @@
+console.log("Hola People");
+
+let cajaDeJu ="Moto";
+console.log(cajaDeJu);
+
+let edad =22;
+
+let nombre ="Jeferson";
+
+console.log("Mi nombre es",nombre, "y tengo",edad,"AÑOS");
+
+//array
+
+let frutas =["manzana verde","guanabana","piña"];
+
+console.log(frutas[1]);
+
+let frase ="JAVAScript es muy increible de los increibles"
+
+console.log(frase);
+console.log(frase.length);
+
+let miTexto = "JAVAScript es muy increible de los increibles";
+let textoEnMayusculas = miTexto.toUpperCase();
+console.log(textoEnMayusculas); // Salida: "Hola people"
+
+let miTexto2 = "JAVAScript es muy increible de los increibles";
+let textoEnMinusculas = miTexto.toLowerCase();
+console.log(textoEnMinusculas); // Salida: "Hola people"
+
+console.log(frase.toUpperCase(frase));
+console.log(frase.toLowerCase(frase));
+
+//operaciones matematicas
+const suma= 13+56;
+console.log(suma);
+
+
+
+
